59fcaf7a8819918631efb8a7f8d32ccd80704e91
Kostya <kostya.permin2007@gmail.com>
2025-05-27 13:13:18 +0500

    add gradient

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,33 @@
 <template>
-    <header class="flex items-center justify-between px-6 py-4 border-b shadow-sm bg-peach">
-        <div class="flex items-center gap-2">
+    <header class="grid grid-cols-3 items-center px-6 py-4 bg-peach shadow-sm hover:shadow-lg transition-all duration-300 border-gradient">
+        <div class="flex items-center gap-2 select-none">
             <img src="assets/images/logo.png" alt="Site Logo" class="w-16 h-16" >
-            <div class="leading-tight">
+            <div class="leading-tight cursor-default select-none">
                 <p class="text-xs text-gray-500">THIS IS</p>
                 <p class="text-sm font-bold">OKAK TEAM</p>
             </div>
         </div>
-        <nav class="flex space-x-8 text-sm mx-auto" aria-label="Main navigation">
-            <NuxtLink to="/" class="p-2 hover:bg-retrogreen rounded transition-colors">Home</NuxtLink> 
-            <NuxtLink to="/Lab3" class="p-2 hover:bg-retrogreen rounded transition-colors">Lab3</NuxtLink>
-            <NuxtLink to="#" class="p-2 hover:bg-retrogreen rounded transition-colors">LogIn</NuxtLink>
-            <NuxtLink to="#" class="p-2 hover:bg-retrogreen rounded transition-colors">LogOut</NuxtLink>
+        <nav class="flex justify-center space-x-8 text-sm" aria-label="Main navigation">
+            <NuxtLink to="/" class="p-2 hover:text-white hover:bg-gradient-to-r from-[#8D0027] to-[#F1A514] rounded transition-colors duration-300">Home</NuxtLink> 
+            <NuxtLink to="/Lab3" class="p-2 hover:text-white hover:bg-gradient-to-r from-[#8D0027] to-[#F1A514] rounded transition-colors duration-300">Lab3</NuxtLink>
+            <NuxtLink to="#" class="p-2 hover:text-white hover:bg-gradient-to-r from-[#8D0027] to-[#F1A514] rounded transition-colors duration-300">LogIn</NuxtLink>
+            <NuxtLink to="#" class="p-2 hover:text-white hover:bg-gradient-to-r from-[#8D0027] to-[#F1A514] rounded transition-colors duration-300">LogOut</NuxtLink>
         </nav>
-        <div class="text-sm">En ▾</div>
+        <div></div>
     </header>
 </template>
+
+<style scoped>
+.border-gradient {
+  border-bottom-width: 2px;
+  border-bottom-style: solid;
+  border-image-slice: 1;
+  border-image-source: linear-gradient(
+    to right,
+    #8D0027,
+    #DA4011,
+    #F1A514,
+    #2DAF91
+  );
+}
+</style>
